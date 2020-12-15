1f4698b3c394dea1f84d72bb25213a834a831bf7
Purva Dhuri <dhuripurva25@gmail.com>
2020-12-16 00:55:58 +0530

    refactor

---
+++ sign.js
@@ -0,0 +1,79 @@
+
+import React, { useState } from "react";
+import "./styles.css";
+var emojidict{
+  ⬆️ ":"Up Arrow",
+  ↗️ ":"Up-Right Arrow",
+  ➡️ ":"Right Arrow",
+  ↘️ ":"Down-Right Arrow",
+  ⬇️ ":"Down Arrow",
+  ↙️ ":"Down-Left Arrow",
+  ⬅️ ":"Left Arrow",
+  ↖️ ":"Up-Left Arrow",
+  ↕️ ":"Up-Down Arrow",
+  ↔️ ":"Left-Right Arrow",
+  "↩️": "Right Arrow Curving Left",
+  "↪️":"Left Arrow Curving Right",
+  "⤴️":" Right Arrow Curving Up",
+  "⤵️":" Right Arrow Curving Down",
+  "🔃":"Clockwise Vertical Arrows",
+  "🔄": "Counterclockwise Arrows Button",
+  "🔙":"Back Arrow",
+  "🔚": "End Arrow",
+  "🔛":" On! Arrow",
+  "🔜": "Soon Arrow",
+  "🔝": "Top Arrow"
+}
+
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h1 style={{color:"blue"}}>Sign Interpreter</h1>
+      <input type="textbox"></input>
+
+  
+</div>
+  );
+}
+import React, { useState } from "react";
+import "./styles.css";
+var emojidict{
+  ⬆️ ":"Up Arrow",
+  ↗️ ":"Up-Right Arrow",
+  ➡️ ":"Right Arrow",
+  ↘️ ":"Down-Right Arrow",
+  ⬇️ ":"Down Arrow",
+  ↙️ ":"Down-Left Arrow",
+  ⬅️ ":"Left Arrow",
+  ↖️ ":"Up-Left Arrow",
+  ↕️ ":"Up-Down Arrow",
+  ↔️ ":"Left-Right Arrow",
+  "↩️": "Right Arrow Curving Left",
+  "↪️":"Left Arrow Curving Right",
+  "⤴️":" Right Arrow Curving Up",
+  "⤵️":" Right Arrow Curving Down",
+  "🔃":"Clockwise Vertical Arrows",
+  "🔄": "Counterclockwise Arrows Button",
+  "🔙":"Back Arrow",
+  "🔚": "End Arrow",
+  "🔛":" On! Arrow",
+  "🔜": "Soon Arrow",
+  "🔝": "Top Arrow"
+}
+
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <h1 style={{color:"blue"}}>Sign Interpreter</h1>
+      <input type="textbox"></input>
+
+  
+</div>
+  );
+}
